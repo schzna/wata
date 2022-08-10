e7bedfe487e45a31cd76374ace479e15dab9bfd8
yuki74w <16777216bit@gmail.com>
2022-08-11 01:29:52 +0900

    feat: タブ切り替え機能追加
    タブ切り替えにより表示するタブを変える機能を追加した。

--- tab/script.js
+++ tab/script.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", (_) => {
     const wd = document.querySelector("window");
-    wd.querySelectorAll("*").forEach((elem) => {});
     wd.querySelectorAll("tab-name").forEach((elem) => {
         function onclick_tabname(event) {
             event.target.setAttribute("selected", "true");
+            document.querySelectorAll(`tab`).forEach((elem) => {
+                if (
+                    elem.getAttribute("id") ===
+                    event.target.getAttribute("tab-id")
+                ) {
+                    elem.setAttribute("selected", true);
+                } else {
+                    elem.setAttribute("selected", false);
+                }
+            });
             wd.querySelectorAll("tab-name").forEach((elem) => {
                 if (elem.innerHTML != event.target.innerHTML) {
                     elem.setAttribute("selected", "false");
                 }
             });
         }
+
         elem.addEventListener("click", onclick_tabname);
     });
 });
